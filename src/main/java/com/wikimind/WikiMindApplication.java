e4a9532a25b8001ce2dc2901414f78b8ec8026a8
Suyash <suyashchamoli1@gmai.com>
2026-09-16 11:34:34 +0530

    basic settup

---
+++ src/main/java/com/wikimind/WikiMindApplication.java
@@ -0,0 +1,13 @@
+package com.wikimind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikiMindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WikiMindApplication.class, args);
+	}
+
+}
